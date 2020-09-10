683d914dd387e5c45797086343b46f61a0d0bff9
yoichimichael <yoichi.michael.nagano@gmail.com>
2020-09-10 14:42:44 -0400

    added jquery script file and source linking tags and tested scripts

---
+++ resources/js/scripts.js
@@ -0,0 +1,9 @@
+jQuery.noConflict()(function ($){
+    $(document).ready(function() {
+
+        $('h1').click(function(){
+    //        $(this).css('background-color', '#ff0000');
+            $(this).hide();
+        })
+    }) 
+})
