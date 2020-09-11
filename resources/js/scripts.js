1b1fd802cacfc558152a85f4c6eb575d0ca22801
yoichimichael <yoichi.michael.nagano@gmail.com>
2020-09-11 17:34:02 -0400

    added sticky nav bar styling

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -1,9 +1,17 @@
 jQuery.noConflict()(function ($){
     $(document).ready(function() {
-
         $('h1').click(function(){
-    //        $(this).css('background-color', '#ff0000');
-            $(this).hide();
+            $(this).css('background-color', '#ff0000');
         })
     }) 
 })
+
+//
+//$(document).ready(function() {
+//    $('h1').click(function(){        
+//        $(this).hide();
+//    })
+//}) 
+//
+//
+
