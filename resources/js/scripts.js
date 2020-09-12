82255c05afdba17962fe79ec02c8770a2e2899a1
yoichimichael <yoichi.michael.nagano@gmail.com>
2020-09-12 14:28:16 -0400

    added scrolling actions to header buttons

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -1,21 +1,36 @@
-jQuery.noConflict()(function ($){
+$(document).ready(function() {
     
-    $('.js--section-features').waypoint(function(direction){
-        if (direction == "down") {
-            $('nav').addClass('sticky')
+    
+                // for sticky navigation
+    $('.js--section-features').waypoint(function (direction) {
+        if (direction === "down") {
+            $('nav').addClass('sticky');
         } else {
-            $('nav').removeClass('sticky')
-        }
-    }, {
-        offset: '60px;'
-    })
+            $('nav').removeClass('sticky');
+        }}, {
+            offset: '60px;'
+    });
+    
+            // for auto-scrolling from button clicks
+    
+        // scrolls to top of .section-plans in 1000ms
+    $('.js--scroll-to-plans').click(function () {
+        $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1000);     
+    });
+    
+    // scrolls to top of .section-features in 1000ms
+    $('.js--scroll-to-start').click(function () {
+        $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000);     
+    });
+    
+});
+
 //    var waypoints = $('#handler-first').waypoint(function(direction) {
 //        notify(this.element.id + ' hit 25% from top of window') 
 //    }, {
 //        offset: '25%'
 //    })
-    
-})
+
 
 
 
@@ -29,12 +44,11 @@ jQuery.noConflict()(function ($){
 //    }) 
 //})
 
-//
 //$(document).ready(function() {
 //    $('h1').click(function(){        
 //        $(this).hide();
 //    })
 //}) 
-//
-//
+
+
 
